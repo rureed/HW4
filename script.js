1c2be7d722f9a30fd368c0e999451663f41a9bea
rureed <rureed@gmail.com>
2020-04-03 18:45:31 -0400

    updating files

---
+++ script.js
@@ -0,0 +1,30 @@
+function countDown() {
+    var timeLeft = 75;
+  
+    var timeInterval = setInterval(function() {
+      timerEl.textContent = timeLeft + " seconds remaining";
+      timeLeft--;
+  
+      if (timeLeft === 0) {
+        timerEl.textContent = "";
+        speedRead();
+        clearInterval(timeInterval);
+      }
+  
+    }, 1000);
+  }
+  
+
+var startBtn = document.get
+
+function start() {
+
+}
+
+function next() {
+
+}
+
+function answer() {
+
+}
