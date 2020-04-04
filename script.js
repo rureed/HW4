415d23f6417036db6d947bd1e9e713020a59f863
rureed <rureed@gmail.com>
2020-04-03 22:49:45 -0400

    updating files

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-function remaining() {
-    var timeLeft = 75;
-  
-    var timeInterval = setInterval(function() {
-      timerEl.textContent = timeLeft + " seconds remaining";
-      timeLeft--;
-  
-      if (timeLeft === 0) {
-        timerEl.textContent = "";
-        speedRead();
-        clearInterval(timeInterval);
-      }
-  
+var score = 0;
+var question = -1;
+var remaining = 0;
+var clock;
+
+function time() {
+    remaining = 75;
+    document.getElementById("remaining").innerHTML = remaining;
+
+    clock = setInterval(function() {
+        remaining--;
+        document.getElementById("remaining").innerHTML = remaining;
+
+        if (remaining <= 0) {
+            clearInterval(clock);
+            endQuiz();
+        }
     }, 1000);
-  }
+    next()
+}
+
+
   
 
 var startBtn = document.get
 
 function start() {
+
 
 }
 
