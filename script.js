4f229b4ea2448c12ef9f40687425e56b1f5eeba1
rureed <rureed@gmail.com>
2020-04-03 23:44:23 -0400

    updating files

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+var questions = [{
+    title: "WORK IT, MAKE IT, DO IT, ...?",
+    choices: ["BUILD IT", "MAKES US", "DAFT", "PUNK"],
+    answer: "MAKES US"
+},
+{
+    title: "HARDER, BETTER, FASTER, ...?",
+    choices: ["SMARTER", "STRONGER", "SMELLY", "DISCOVERY"],
+    answer: "STRONGER"
+},
+{
+    title: "MORE THAN, HOUR, OUR, ...?",
+    choices: ["FLOWER", "NEVER", "INTERSTELLA", "5555"],
+    answer: "NEVER"
+},
+{
+    title: "EVER, AFTER, WORK IS, ...?",
+    choices: ["HELMETS", "OVER", "THOMAS", "GUY-MANUEL"],
+    answer: "OVER"
+},
+{
+    title: "Work it harder, make it better, do it faster, makes us stronger, more than ever, hour after, our work is ...?",
+    choices: ["always coding", "never over", "one more time", "TRON"],
+    answer: "never over"
+}
+]
+
 var score = 0;
 var question = -1;
 var remaining = 0;
